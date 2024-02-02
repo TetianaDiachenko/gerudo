27261d1a326e43c51ec602674f228c275200a8f1
TetianaDiachenko <tetianamyhun@gmail.com>
2024-02-02 15:50:53 +0200

    scroll for fixed header

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,14 @@ if (iconMenu) {
 		
 	});
 }
+
+//Scrolled
+const header = document.querySelector('.header');
+document.addEventListener('scroll', () => {
+	if(window.scrollY > 0) {
+		header.classList.add('scrolled');
+
+	} else {
+		header.classList.remove('scrolled');
+	}
+})

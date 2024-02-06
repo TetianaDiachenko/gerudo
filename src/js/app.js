1bcbff0ed045e0abcb22352633ec93a3ae015902
TetianaDiachenko <tetianamyhun@gmail.com>
2024-02-06 18:15:27 +0200

    header instead document.querySelector('.header')

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ if(menuLinks.length > 0) {
 		const menuLink = e.target;
 		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.header').offsetHeight;
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - header.offsetHeight;
 
 			if(iconMenu.classList.contains('_active')) {
 				document.body.classList.remove('_lock');
